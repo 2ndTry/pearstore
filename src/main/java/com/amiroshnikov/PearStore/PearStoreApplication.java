e737ecf0167aad7552c91e927eca08a645069a98
2ndTry <mirnijalex@gmail.com>
2022-03-06 10:14:26 +0300

    Test

--- src/main/java/com/amiroshnikov/PearStore/PearStoreApplication.java
+++ src/main/java/com/amiroshnikov/PearStore/PearStoreApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PearStoreApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(PearStoreApplication.class, args);
 	}
